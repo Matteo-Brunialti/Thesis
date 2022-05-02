87ec3a12fac0c8ddc2e7788b7f210c7ca4862801
Matteo-Brunialti <79841435+Matteo-Brunialti@users.noreply.github.com>
2022-05-02 12:17:50 +0200

    Delete architecture.py

--- architecture.py
+++
@@ -1,7 +0,0 @@
-import os
-
-l = ['mydata', 'mydata/tmp', 'mydata/augustus', 'mydata/gffread', 'mydata/gffread/cds', 'mydata/gffread/tr_cds', 'mydata/Orthofinder']
-
-for test in l:
-    if not os.path.exists(test):
-        os.makedirs(test)
